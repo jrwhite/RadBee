f16ac7133b3a2f542ef5a4bbae43a47803bf5c4c
jrwhite <jrwhite20@gmail.com>
2018-05-07 03:50:16 -0500

    first

---
+++ Makefile
@@ -0,0 +1,11 @@
+BINARY = src/disco/main
+
+# OBJS = common/comm.o src/HAL.o common/neuron.o izhi/izhi.o
+
+OPENCM3_DIR = libopencm3
+
+BIN_DIR = bin
+
+LDSCRIPT = src/disco/stm32f4-disco.ld
+
+include src/libopencm3.target.mk
---
+++ src/disco/main.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
+#include <libopencm3/usb/usbd.h>
+#include <libopencm3/usb/cdc.h>
+#include <libopencm3/cm3/scb.h>
+
+
+int main(void) {
+
+    /* Initialize system */
+	rcc_clock_setup_hse_3v3(&rcc_hse_8mhz_3v3[RCC_CLOCK_3V3_120MHZ]);
+
+	rcc_periph_clock_enable(RCC_GPIOA);
+	rcc_periph_clock_enable(RCC_OTGFS);
+
+	gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE,
+			GPIO9 | GPIO11 | GPIO12);
+	gpio_set_af(GPIOA, GPIO_AF10, GPIO9 | GPIO11 | GPIO12);
+
+	while (1) {
+    }
+}
